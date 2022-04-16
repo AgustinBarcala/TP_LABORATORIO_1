742df0be4e11e300d17d92d7f72c3f38d19763e1
AgustinBarcala <Acuna7960@gmail.com>
2022-04-16 17:44:28 -0300

    Trabajo ya documentado

--- tp[1]/src/calcular.h
+++ tp[1]/src/calcular.h
@@ -8,7 +8,10 @@
 #ifndef CALCULAR_H_
 #define CALCULAR_H_
 
-
+/// @fn int calcularCostos (int* kilometros, float precio1, float precio2, int flag).
+/// @brief Calcula los costos de los vuelos y los muestra.
+/// @param kilometros Valor que ingresa el usuario, precio1 Valor del primer precio, precio2 Segundo precio, flag Determina si calcula o muestra.
+/// @return Un numero del tipo int para determinar si la funcion fue o no exitosa.
 int calcularCostos (int* kilometros,float* precio1, float* precio2, int flag);
 
 
